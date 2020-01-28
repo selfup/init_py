5097657dc781c939a68f120484d694e69dd62446
selfup <boudinot.regis@yahoo.com>
2020-01-28 02:45:26 -0600

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import requests
 
-result = requests.get("https://example.com/")
+result_switch = {
+    200: "OK",
+    404: "NOT FOUND",
+}
 
-print(result.status_code)
+result = requests.get("https://example.com/")
+status_code = result.status_code
+
+status = result_switch.get(status_code)
+
+if status != None:
+    print(status)
+else:
+    print("UH OHH")
